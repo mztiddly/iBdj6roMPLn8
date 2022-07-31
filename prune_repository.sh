a4af04e76d72ed5728c443e4bf07c1ce021a334f
May Bee <may.bee@123.plirp>
2022-07-31 18:33:01 +0200

    update pruning script (again)

--- prune_repository.sh
+++ prune_repository.sh
@@ -1,30 +1,38 @@
 #! /usr/bin/bash
 BACKUPDIR="./backup/$(date -Idate)"
 
-if [ -d "$BACKUPDIR" ]; then
-    echo
-    echo "Output directory \"$BACKUPDIR\" already exists."
-    echo
-
-    exit 1
-fi
-
 if [ -n "$(git status --porcelain)" ]; then
     echo
     echo "Found uncommitted changes."
     echo
 
     exit 1
-    echo "there are changes";
 fi
 
-mkdir -p "$BACKUPDIR"
+if [ -d "$BACKUPDIR" ]; then
+    echo
+    echo "Output directory \"$BACKUPDIR\" already exists."
+    echo
 
-git pull
+    exit 1
+else
+    mkdir -p "$BACKUPDIR"
+fi
 
+# get latest changes from remote repository
+if ! git pull; then
+    echo
+    echo "Git pull returned a non-zero exit code."
+    echo
+
+    exit 1
+fi
+
+# backup local repository
 mv ".git" "$BACKUPDIR/"
 cp "index.html" "$BACKUPDIR/"
 
+# create new local repository
 git init
 git status
 
@@ -33,13 +41,16 @@ git config --local commit.gpgsign false
 git config --local user.name "May Bee"
 git config --local user.email "may.bee@123.plirp"
 
+# add relevant files
 git add .
 git add -f index.html
 git status
 
+# create single commit from current data
 git commit -m "new root commit"
 git log
 
+# upload to remote repository
 git branch -m "master" "main"
 git remote add origin  https://github.com/mztiddly/iBdj6roMPLn8.git
 git push --force --set-upstream origin main
